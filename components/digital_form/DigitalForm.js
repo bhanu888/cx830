98720a423d180f3e4bd13fce1635be8748959980
ferrariturbo <Andy-Chan@Yuipangs-MacBook-Pro.local>
2018-08-31 12:05:22 +0800

    Add fields for digital forms
    Former-commit-id: 0d71869dc7ba7c4697bb87198ba04b2bcf34a2da

--- components/digital_form/DigitalForm.js
+++ components/digital_form/DigitalForm.js
@@ -18,17 +18,40 @@ export default class DigitalForm extends Component {
   }
 
   render() {
+
+    console.log(this.state.data);
+
     return (
       <ScrollView style={GlobalStyles.container}>
+
         <View style={GlobalStyles.flatListSummaryItem}>
           <Text style={GlobalStyles.flatListTitle}>{this.state.data.FirstName} {this.state.data.LastName}</Text>
           <Text style={GlobalStyles.flatListDescription}>Name</Text>
         </View>
 
         <View style={GlobalStyles.flatListSummaryItem}>
+            <Text style={GlobalStyles.flatListTitle}>{this.state.data.Company}</Text>
+            <Text style={GlobalStyles.flatListDescription}>Company</Text>
+        </View>
+
+        <View style={GlobalStyles.flatListSummaryItem}>
           <Text style={GlobalStyles.flatListTitle}>{this.state.data.Address.street}, {this.state.data.Address.city} {this.state.data.Address.country}</Text>
           <Text style={GlobalStyles.flatListDescription}>Address</Text>
         </View>
+
+        <View style={GlobalStyles.flatListSummaryItem}>
+            <Text style={GlobalStyles.flatListTitle}>{this.state.data.Email}</Text>
+            <Text style={GlobalStyles.flatListDescription}>Email</Text>
+        </View>
+
+        <View style={GlobalStyles.flatListSummaryItem}>
+            <Text style={GlobalStyles.flatListTitle}>{this.state.data.MobilePhone}</Text>
+            <Text style={GlobalStyles.flatListDescription}>Mobile</Text>
+        </View>
+
+
+
+           
       </ScrollView>
     );
   }
