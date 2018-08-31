d1ac9e8eb9a0dbecad6057830e1f358981d992c2
Russell <russell.cheung@aexp.com>
2018-08-31 11:54:13 +0800

    Update lead view
    Former-commit-id: 6faf429c0d55aa8c65caf121fa38a720cf9969a9

--- components/agenda/AgendaScreen.js
+++ components/agenda/AgendaScreen.js
@@ -4,49 +4,92 @@ import {
   Text,
   View,
   FlatList,
+  ScrollView,
 } from 'react-native';
+import {AnimatedGaugeProgress, GaugeProgress} from 'react-native-simple-gauge';
 import {oauth, net} from 'react-native-force';
 import {GlobalStyles} from '../layout/GlobalStyles';
 
 export default class AgendaScreen extends Component {
+  static navigationOptions = {
+    title: 'Dashboard'
+  };
+
   constructor(props) {
     super(props);
     this.state = {data: []};
-    console.log(props)
-
+    this.date = new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
   }
 
   componentDidMount() {
     var that = this;
-    oauth.getAuthCredentials(
-      () => that.fetchData(), // already logged in
-      () => {
-        oauth.authenticate(
-          () => that.fetchData(),
-          (error) => console.log('Failed to authenticate:' + error)
-        );
-      });
-    }
-
-    fetchData() {
-      var that = this;
-      net.query('SELECT Id, Name FROM User LIMIT 10',
-      (response) => that.setState({data: response.records})
-    );
   }
 
   render() {
     return (
-      <View style={GlobalStyles.container}>
-      <FlatList
-        data={this.state.data}
-        renderItem={({item}) => <Text style={GlobalStyles.flatListItem}>{item.Name}</Text>}
-        keyExtractor={(item, index) => 'key_' + index}
-      />
-      </View>
+      <ScrollView style={GlobalStyles.container}>
+        <View style={styles.container}>
+          <AnimatedGaugeProgress
+            style={styles.gauge}
+            size={150}
+            width={15}
+            fill={80}
+            rotation={90}
+            cropDegree={120}
+            tintColor="#4682b4"
+            delay={0}
+            backgroundColor="#b0c4de"
+            stroke={[2, 2]} //For a equaly dashed line
+            strokeCap="circle" />
+
+          <Text style={GlobalStyles.header}>{this.date}</Text>
+          <Text style={GlobalStyles.subheader}>Today</Text>
+        </View>
+        <View style={GlobalStyles.flatListSummaryItem}>
+          <Text style={GlobalStyles.flatListTitle}>Patricia Feager - Vogue Laundry</Text>
+          <Text style={GlobalStyles.flatListDescription}>Meeting - 12:30 PM</Text>
+        </View>
+        <View style={GlobalStyles.flatListSummaryItem}>
+          <Text style={GlobalStyles.flatListTitle}>Andy Chan - Oi Tung Shopping Centre</Text>
+          <Text style={GlobalStyles.flatListDescription}>Call - 9:00 AM-10:00 AM</Text>
+        </View>
+        <View style={GlobalStyles.flatListSummaryItem}>
+          <Text style={GlobalStyles.flatListTitle}>Sandra Eberhard - Ricacorp Properties</Text>
+          <Text style={GlobalStyles.flatListDescription}>Call - 1:00 AM-2:00 PM</Text>
+        </View>
+
+        <View style={GlobalStyles.container}>
+          <Text style={GlobalStyles.subheader}>Upcoming</Text>
+        </View>
+
+        <View style={GlobalStyles.flatListSummaryItem}>
+          <Text style={GlobalStyles.flatListTitle}>Jeff Glimpse - Harbour Plaza North Point</Text>
+          <Text style={GlobalStyles.flatListDescription}>Call - 02/09/18 2:00-3:00 PM</Text>
+        </View>
+        <View style={GlobalStyles.flatListSummaryItem}>
+          <Text style={GlobalStyles.flatListTitle}>Kristen Akin - APITA</Text>
+          <Text style={GlobalStyles.flatListDescription}>Meeting - 03/09/18 4:00 PM</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingBottom:8,
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  gauge: {
+    padding: 4,
+  },
 });
